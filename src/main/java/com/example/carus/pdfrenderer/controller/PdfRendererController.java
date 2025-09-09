522d3d0b737cfc47b54d8e27aa0a3557eca1445e
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-09-09 18:46:05 +0300

    feat: add flying saucer pdf generation cs-1

--- src/main/java/com/example/carus/pdfrenderer/controller/PdfRendererController.java
+++ src/main/java/com/example/carus/pdfrenderer/controller/PdfRendererController.java
@@ -1,14 +1,23 @@
 package com.example.carus.pdfrenderer.controller;
 
 import com.example.carus.pdfrenderer.dto.PdfRendererRequestDto;
+import com.example.carus.pdfrenderer.service.PdfRendererService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class PdfRendererController {
+    private  final PdfRendererService service;
+
+    PdfRendererController(PdfRendererService service) {
+        this.service = service;
+    }
+
     @PostMapping("/pdf-render")
-    public PdfRendererRequestDto pdfRender(@RequestBody PdfRendererRequestDto body) {
-        return new PdfRendererRequestDto(String.format("This is your html %s!", body.content()));
+    public ResponseEntity<byte[]> pdfRender(@RequestBody PdfRendererRequestDto body) {
+        return service.pdfRender(body.content());
     }
 }
--- src/main/java/com/example/carus/pdfrenderer/service/PdfRendererService.java
+++ src/main/java/com/example/carus/pdfrenderer/service/PdfRendererService.java
@@ -1,4 +1,45 @@
 package com.example.carus.pdfrenderer.service;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.xhtmlrenderer.layout.SharedContext;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+@Service
 public class PdfRendererService {
+    public ResponseEntity<byte[]> pdfRender(String html) {
+        Document document = Jsoup.parse(html);
+        document.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            ITextRenderer renderer = new ITextRenderer();
+
+            SharedContext sharedContext = renderer.getSharedContext();
+            sharedContext.setPrint(true);
+            sharedContext.setInteractive(false);
+
+            renderer.setDocumentFromString(document.html());
+            renderer.layout();
+            renderer.createPDF(outputStream);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_PDF);
+            final String filename = "rendered-document.pdf";
+            headers.setContentDispositionFormData(filename, filename);
+            headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+
+            return new ResponseEntity<>(outputStream.toByteArray(), headers, HttpStatus.OK);
+
+        } catch (IOException e) {
+            System.err.println("Error generating PDF: " + e.getMessage());
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
