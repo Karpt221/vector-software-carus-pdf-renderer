08e1b4aba9ba2b4cebcb87302a3c84be0b0742ae
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-09-10 19:55:12 +0300

    fix: add html validation exception cs-1

---
+++ src/main/java/com/example/carus/pdfrenderer/exceptions/HtmlValidationException.java
@@ -0,0 +1,7 @@
+package com.example.carus.pdfrenderer.exceptions;
+
+public class HtmlValidationException extends RuntimeException {
+    public HtmlValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
