567fe3e98371ffdf04e65c79a44421f6069fda7a
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-09-21 19:15:41 +0300

    fix: handle malformed body exceptions  cs-1

--- src/main/java/com/example/carus/pdfrenderer/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/carus/pdfrenderer/exceptions/GlobalExceptionHandler.java
@@ -4,9 +4,12 @@ import com.example.carus.pdfrenderer.services.RequestLoggerService;
 import com.example.carus.pdfrenderer.exceptions.exceptions.HtmlValidationException;
 import com.example.carus.pdfrenderer.exceptions.exceptions.PdfGenerationException;
 import com.example.carus.pdfrenderer.utils.dtos.ErrorResponseDto;
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -24,8 +27,19 @@ public class GlobalExceptionHandler {
         return handleErrorResponse(HttpStatus.BAD_REQUEST, ex);
     }
 
-    @ExceptionHandler({PdfGenerationException.class, Exception.class})
+    @ExceptionHandler(PdfGenerationException.class)
     public ResponseEntity<ErrorResponseDto> handlePdfGenerationException(PdfGenerationException ex) {
+        return handleErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex);
+    }
+
+    @ExceptionHandler({HttpMessageNotReadableException.class, MethodArgumentNotValidException.class,
+            UnrecognizedPropertyException.class})
+    public ResponseEntity<ErrorResponseDto> handleMalformedRequestExceptions(Exception ex) {
+        return handleErrorResponse(HttpStatus.BAD_REQUEST, ex);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleOtherExceptions(Exception ex) {
         return handleErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex);
     }
 
@@ -33,7 +47,7 @@ public class GlobalExceptionHandler {
         log.error("An error occurred while processing", ex);
         dbLogger.logErrorRequest(ex.toString());
         return new ResponseEntity<>(
-                new ErrorResponseDto(status, ex.getMessage()),
+                new ErrorResponseDto(status, ex.getMessage(), ex.getCause() == null ? "" : ex.getCause().getMessage()),
                 status
         );
     }
--- src/main/java/com/example/carus/pdfrenderer/utils/dtos/ErrorResponseDto.java
+++ src/main/java/com/example/carus/pdfrenderer/utils/dtos/ErrorResponseDto.java
@@ -2,4 +2,4 @@ package com.example.carus.pdfrenderer.utils.dtos;
 
 import org.springframework.http.HttpStatus;
 
-public record ErrorResponseDto(HttpStatus status, String message) { }
+public record ErrorResponseDto(HttpStatus status, String message, String cause) { }
